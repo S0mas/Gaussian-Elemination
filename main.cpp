58467b19d8b1325479733af2102510576bb86f2f
S0mas <sommerfeld.krzysztof@gmail.com>
2015-06-23 20:03:12 +0200

    Pawel - 3.0

--- main.cpp
+++ main.cpp
@@ -1,24 +1,52 @@
 #include <iostream>
 using namespace std;
 
-void Gaussian_Elimination(double** MA, int N)
+int podaj_i_sprawdz_wartosc()
 {
+    int wartosc;
+    while(!(cin>>wartosc))
+    {
+        cout<<"Blad. Niepoprawna wartosc\n";
+        cin.clear();
+        cin.ignore( 1000, '\n' );
+    }
+    return wartosc;
+}
+
+int start()
+{
+        int N;//numbersOfUnknowns
+        cout<<"Podaj ilosc niewiadomych w rownaniach"<<endl;
+        while(!(cin>>N)||N<=0)
+        {
+            cout<<"Blad. Liczba niewiadomych musi byc wieksza od 0\n";
+            cin.clear();
+            cin.ignore( 1000, '\n' );
+        }
+        return N;
+}
+
+
+void Gaussian_Elimination(int N)
+{
+    double** MA = 0;//coefficientMatrix
     int i,j,k; //Index
     double D;//Divisor
+    int temp;
     ///////////////////////////////////////////////////DEKLARACJA DYNAMICZNEJ TABLICY
     MA = new double *[N];
     for(i = 0; i < N; i++)
     {
         MA[i] = new double [N+2];
     }
-    //////////////////////////////////////////////////ZBIERA DANE POTRZEBNE DO ROZWIAZANIA ZADAWANIA
+    //////////////////////////////////////////////////ZBIERA DANE POTRZEBNE DO ROZWIAZANIA ZADANIA
     for(i = 0; i < N; i++)
     {
         for(j = 1; j <= N+1; j++)
         {
-            if(j<=N)cout<<"Podaj współczynnik zmiennej nr "<<j<<" w równaniu nr "<<i+1<<endl;
-            else cout<<"Podaj wynik równania nr "<<i+1<<endl;
-            cin>>MA[i][j];
+            if(j<=N)cout<<"Podaj wspolczynnik zmiennej nr "<<j<<" w rownaniu nr "<<i+1<<endl;
+            else cout<<"Podaj wynik rownania nr "<<i+1<<endl;
+            MA[i][j]=podaj_i_sprawdz_wartosc();
         }
     }
     ///////////////////////////////////////////////////WYSWITLA WPISANE DANE W POSTACI MACIERZY
@@ -34,9 +62,18 @@ void Gaussian_Elimination(double** MA, int N)
     ///////////////////////////////////////////////////ROZWIAZUJE MACIERZ ZA POMOCA ELIMINACJI GAUSSA
     for(i=0; i<N-1; i++)
     {
-        j=i+1;
-        for(j; j<N; j++)
+        for(j=i+1; j<N; j++)
         {
+            if(MA[i][i+1]==0)////////////////////////////ZAMIENIA KOLEJNOŚĆ WIERSZY W PRZYPADKU DZIELENIA PRZEZ ZERO
+            {
+                for(k=1; k<N+2; k++)
+                {
+                    temp = MA[i+1][k];
+                    MA[i+1][k] = MA[i][k];
+                    MA[i][k] = temp;
+                }
+
+            }
             D=MA[j][i+1]/MA[i][i+1];
             for(k=1; i+k<=N+1; k++)
             {
@@ -79,15 +116,33 @@ void Gaussian_Elimination(double** MA, int N)
             cout<<MA[i][j]<<"\t";
         }
     }
+    delete[] MA;
 }
+void opcje()
+{
+    cout<<endl<<endl;
+    cout<<"\t1 - dodaj rownanie"<<endl;
+    cout<<"\tInna Liczba - zakoncz program"<<endl;
+}
+
 int main()
 {
-    double** MA;//coefficientMatrix
-    int N;//numbersOfUnknowns
-    cout<<"Podaj ilość niewiadomych w równaniach"<<endl;
-    cin>>N;
-    Gaussian_Elimination(MA,N);
-    delete[] MA;
+    bool exit = false;
+    cout<<endl<<endl<<"WITAJ W OBLICZARCE ROWNAN LINIOWYCH METODA GAUSSA"<<endl<<endl;
+    do
+    {
+        opcje();
+        switch (podaj_i_sprawdz_wartosc())
+        {
+            case 1:
+                Gaussian_Elimination(start());
+                break;
+            default:
+                exit = true;
+                cout<<endl<<"Zakonczono";
+                break;
+        }
+    }while(exit == false);
     return 0;
 }
 
